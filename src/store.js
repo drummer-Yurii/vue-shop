4a400f26884cd5e0fff137c3b383323ac0e5d575
Yurii Rybachok <drummer2009@ukr.net>
2023-10-03 15:29:45 +0300

    create store module cart

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 import product from './store/modules/product';
+import cart from './store/modules/cart';
 
 const store = createStore({
-    modules: {
-        prods: product
-    }
+  modules: {
+    prods: product,
+    crd: cart,
+  },
 });
 
 export default store;
